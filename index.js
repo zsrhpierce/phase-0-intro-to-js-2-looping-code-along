5a88d099df25606a51f9a9952d7f6eaac555a87d
Cathy Pierce <zsrhpierce@gmail.com>
2023-08-06 21:46:43 -0600

    for

--- index.js
+++ index.js
@@ -1,46 +1,27 @@
-// for ([initialization]; [condition]; [iteration]) {
-//     [loop body]
-// }
-
-// Usage: Use a for loop when you know how many times you want the loop to run (for example, when you're looping through elements in an array)
-
-
-// const gifts = ["teddy bear", "drone", "doll"];
-
-// function wrapGifts(gifts) {
-//     for (let i = 0; i < gifts.length; i++) {
-//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
-
-//     }
-
-//     return gifts;
-// }
-
-// wrapGifts(gifts);
-
-
-function writeCards(names, event) {
-    let messages = []
-    for (let i = 0; i < names.length; i++) {
-        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-
+function writeCards(namesArray, eventName) {
+    const cards = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      const cardMessage = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      cards.push(cardMessage);
+     
     }
-    return messages;
-}
-// const gifts = ['teddy bear', 'drone', 'doll'];
-// function wrapGifts(gifts) {
-//     let i = 0;
-//     while (i < gifts.length) {
-//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
-//         i++;
-//     }
-//     return gifts;
-// }
-// wrapGifts(gifts);
+  
+    return cards;
+  }
+  const namesArray = ['Charlie', 'Samip', 'Ali'];
+const eventName = 'birthday';
 
-function countDown() {
-    let number = 10;
-    while (number >= 0) {
-        console.log(number--);
-    }
+const cards = writeCards(namesArray, eventName);
+
+console.log(cards);
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
 }
+
+
+countDown(10);
